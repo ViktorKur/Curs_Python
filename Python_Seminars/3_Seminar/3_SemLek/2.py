782881b13ac22d763bbca7b53b31324cc59f98e9
Viktor K <popvik13@mail.ru>
2023-04-16 02:26:35 +0300

    Dobavlena zadacha 10 v papku seminara 2 Home Work

---
+++ Python_Seminars/3_Seminar/3_SemLek/2.py
@@ -0,0 +1,37 @@
+# # Напишите программу для печати всех уникальных значений в словаре.
+# list_dict = [{"V": "S001"}, {"V": "S002"},
+#              {"VI": "S001"}, {"VI": "S005"},
+#              {"VII": " S005 "}, {"V": " S009 "},
+#              {"VIII": " S007 "}]
+
+list_dict = [{"V": "S001"}, {"V": "S002"},
+             {"VI": "S001"}, {"VI": "S005"},
+             {"VII": " S005 "}, {"V": " S009 "},
+             {"VIII": " S007 "}]
+
+
+n_list = []
+for i in list_dict:
+    w_list = list(i.values())
+    word = w_list[0]
+    word_clear = word.strip()
+    n_list.append(word_clear)
+
+# print(set(n_list))
+
+# dict = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII ":" S007 "}]
+# result_set = set()
+
+# for i in dict:
+#     for j in i:
+#         result_set.add(i[j])
+
+
+# list_dict = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, 
+#              {"VI": "S005"}, {"VII": " S005 "}, 
+#              {" V ":" S009 "}, {" VIII ":" S007 "}]
+# unique_dict_items = set()
+# for i in list_dict:
+#     for j in i.keys():
+#         unique_dict_items.add(i[j])
+# # print(unique_dict_items)
